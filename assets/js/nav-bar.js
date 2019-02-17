5edc233db06340f4dd51dfd913b927cd7437626d
dte005 <danieldts2013@gmail.com>
2019-02-17 01:07:03 -0300

    nav-bar modifications commit

---
+++ assets/js/nav-bar.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  if (document.getElementById("item1").style.visibility == "hidden"){
+    document.getElementById("item1").style.visibility = "visible";
+  }else{
+    document.getElementById("item1").style.visibility = "hidden";
+  }
+  if (document.getElementById("item2").style.visibility == "hidden"){
+    document.getElementById("item2").style.visibility = "visible";
+  }else{
+    document.getElementById("item2").style.visibility = "hidden";
+  }
+  if (document.getElementById("item3").style.visibility == "hidden"){
+    document.getElementById("item3").style.visibility = "visible";
+  }else{
+    document.getElementById("item3").style.visibility = "hidden";
+  }
+}
